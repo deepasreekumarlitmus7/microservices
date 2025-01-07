5ea179d8e24b68e6d6857dfe576a8845c90f4a4f
deepa.sreekumar <deepa.sreekumar@litmus7.com>
2025-01-07 13:17:23 +0530

    Initial commit

--- src/main/java/com/springboot/litmus7/orderservice/entity/Order.java
+++ src/main/java/com/springboot/litmus7/orderservice/entity/Order.java
@@ -3,7 +3,6 @@ package com.springboot.litmus7.orderservice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import org.hibernate.id.IdentityGenerator;
 
 @Entity
 public class Order {
@@ -11,7 +10,6 @@ public class Order {
     @Id
     @GeneratedValue
     private int orderId;
-
 
 
 }
